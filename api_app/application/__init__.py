ef26dd00143de914040814d2c2e4bb2f540b8d53
sanjaysanju618 <sanjaybabu618@gmail.com>
2020-11-24 13:40:56 +0530

    Removed CORS from API

--- api_app/application/__init__.py
+++ api_app/application/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_restplus import Api
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 api = Api(
     app=app,
-    doc='/',
+    doc="/",
     default_label="Calculator",
     default="Calculator",
 )
+CORS(app)
 
 from application import route
